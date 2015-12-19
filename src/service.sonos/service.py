68011856c49975f649b27011c10ba8cdc7c81da8
Sebastian Brandt <bastibrandt@gmail.com>
2015-12-19 21:56:17 +0100

    Volume will be resetted when playback ends or stops

--- src/service.sonos/service.py
+++ src/service.sonos/service.py
@@ -24,24 +24,46 @@ class MyPlayer(xbmc.Player):
 
 	def onPlayBackStarted( self ):
 		settings = selfAddon.getSetting
-		player_ip = settings('SONOS_PLAYER_IP')
 
 		switch_when_playing = settings('SWITCH_WHEN_PLAYING')
-		set_volume = settings('SET_VOLUME')	
+		set_volume = settings('SET_VOLUME')
 		sonos_volume = int(float(settings('SONOS_VOLUME')))
-	
+
+		sonos = getSonos()
 		xbmc.log("SONOS: Playback started")
-		sonos = SoCo("%s" % player_ip)
-		xbmc.log("SONOS: created soco with ip: %s" % player_ip)
 		xbmc.log("SONOS: switched to line in: %s" % sonos.switch_to_line_in())
 		xbmc.log("SONOS: play: %s" % sonos.play())
 
-		if(set_volume):			
+		if(set_volume):
 			sonos.volume(sonos_volume)
 			xbmc.log("SONOS: set volume to %s" % sonos_volume)
+	def onPlayBackEnded(self):
+		settings = selfAddon.getSetting
+		set_volume = settings('SET_VOLUME')
+		if(set_volume):
+			sonos = getSonos()
+			sonos.volume(0)
+
+	def onPlayBackStopped(self):
+		settings = selfAddon.getSetting
+		set_volume = settings('SET_VOLUME')
+		if(set_volume):
+			sonos = getSonos()
+			sonos.volume(0)
+
+def getSonos():
+	settings = selfAddon.getSetting
+	player_ip = settings('SONOS_PLAYER_IP')
+
+	sonos = SoCo("%s" % player_ip)
+	xbmc.log("SONOS: created soco with ip: %s" % player_ip)
+	return sonos
+
 
 player = MyPlayer()
+monitor = xbmc.Monitor()
 xbmc.log("SONOS: Started and running.")
 
-while(not xbmc.abortRequested):
-    xbmc.sleep(100)
+while True:
+    if monitor.waitForAbort(1): # Sleep/wait for abort for 1 second.
+        break # Abort was requested while waiting. Exit the while loop.
